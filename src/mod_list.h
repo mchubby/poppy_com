f618de0b47f8ad590d8a28a57e7a568503d0c128
nicolas-rabault <nicolas.rabbault@inria.fr>
2016-11-09 15:04:54 +0100

    update mod_list

--- src/mod_list.h
+++ src/mod_list.h
@@ -36,14 +36,20 @@ enum {
     SYNTHE_ADDR,
     IMU_ADDR,
     VIBRATOR_ADDR,
-    SERVO_ADDR
+    SERVO_ADDR,
+    MAX_ADDR
 }addr_register_t;
 //*****************Module registers****************************
 
 typedef enum {
     MASTER_GET_MIC_LEVEL, // uint16_t level
     MASTER_GET_TACTIL_LEVEL, // uint16_t level
-    MASTER_GET_IMU_LEVEL // {float yow, float pitch, float roll, int16_t accelx, int16_t accely, int16_t accelz} (float = 4 bytes)
+    MASTER_GET_PHOTORESISTOR_LEVEL, // uint16_t level
+    MASTER_GET_BAT_LEVEL, // uint16_t level
+    MASTER_GET_IMU_LEVEL, // {float yow, float pitch, float roll, int16_t accelx, int16_t accely, int16_t accelz} (float = 4 bytes)
+    MASTER_SEND_SENSORS_LEVEL, // Send all senslor values (MIC_LEVEL, TACTIL_LEVEL, PHOTORESISTOR_LEVEL, IMU_LEVEL)
+    MASTER_AUTOSEND_DELAY_MS, // a char delay in ms (0 = autosend disable)
+    MASTER_SET_LEDS_COLOR // a char containing on off for colors {0 0 b2 g2 r2 b1 g1 r1}
 }master_register_t;
 
 typedef enum {
@@ -51,7 +57,7 @@ typedef enum {
 }photoresistor_register_t;
 
 typedef enum {
-    TOUCH_LEVEL // request with no data, ask for a reply
+    TACTIL_LEVEL // request with no data, ask for a reply
 }tactil_register_t;
 
 typedef enum {
@@ -67,7 +73,7 @@ typedef enum {
 }mic_register_t;
 
 typedef enum {
-    SYNTHE_VOL,
+    SYNTHE_VOL, //TODO
     PLAY_TEST1, // no data, just play the melody
     PLAY_TEST2, // no data, just play the melody
 }synthe_register_t;
@@ -77,12 +83,12 @@ typedef enum {
 }imu_register_t;
 
 typedef enum {
-    VIBRATOR_SPEED
+    VIBRATOR_SPEED // a char data 0 = no speed
 }vibrator_register_t;
 
 typedef enum {
-    SERVO_LEFT_POSITION,
-    SERVO_RIGHT_POSITION
+    SERVO_LEFT_POSITION, // a char with angle detween 0 and 180
+    SERVO_RIGHT_POSITION // a char with angle detween 0 and 180
 }servo_register_t;
 
 
